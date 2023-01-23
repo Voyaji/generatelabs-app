f3029fbf1549acc6fa78f3508b33d70a859e6907
Lyzer <mercklyzer@gmail.com>
2023-01-23 22:00:20 +0800

    Add Development Section

--- sections/Development.tsx
+++ sections/Development.tsx
@@ -2,61 +2,42 @@ import React, { FunctionComponent } from 'react'
 import Padding from '../components/Padding'
 
 const Development: FunctionComponent = () => {
+
     return (
         <section className='bg-black'>
             <Padding className='space-y-20 py-24'>
                 <h1 className='text-white text-center font-spaceGrotesk text-6xl font-bold'>DEVELOPMENT</h1>
 
-                <div className='flex space-x-2'>
-                <div className='flex-1 space-y-4'>
-                        {/* card */}
-                        <div className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
+                <div className='flex w-full justify-center h-auto relative
+                    after:absolute after:content-[" "] after:w-[5px] after:h-full after:bg-gradient-to-b after:bg-line-gradient'>
+                    <ul className='text-white'>
+                        
+                        <li className='w-1/2 h-auto px-4 py-8 border border-white rounded-lg relative z-[99] mb-8 float-left clear-right -translate-x-7'>
                             <h2 className='font-bold text-2xl'>The Software</h2>
+                            <span className='border border-black w-6 h-6 bg-white rounded-full absolute top-1/2 -translate-y-1/2 -right-[14px] translate-x-7
+                                after:content-[" "] after:bg-blackText after:w-4 after:h-4 after:rounded-full
+                                after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2'></span>
                             <div>A No Code Tool for Creators to be able to build , create and generate NFTs. Experience a seamless transition into NFTs and Web3 using the Generate Labs Software Application.</div>
-                        </div>
+                        </li>
 
-                        <div className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
+                        <li className='w-1/2 h-auto px-4 py-8 border border-white rounded-lg relative z-[99] mb-8 float-right clear-left translate-x-7'>
                             <h2 className='font-bold text-2xl'>Build</h2>
+                            <span className='border border-black w-6 h-6 bg-white rounded-full absolute top-1/2 -translate-y-1/2 -left-[14px] -translate-x-7'></span>
                             <div>Envision your ideas and seamlessly build the NFT Collection and Creation that you have in mind. The possibilities are endless using the Generate Labs No Code Tool and Software.</div>
-                        </div>
+                        </li>
 
-                        <div className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
+                        <li className='w-1/2 h-auto px-4 py-8 border border-white rounded-lg relative z-[99] mb-8 float-left clear-right -translate-x-7'>
                             <h2 className='font-bold text-2xl'>Create</h2>
+                            <span className='border border-black w-6 h-6 bg-white rounded-full absolute top-1/2 -translate-y-1/2 -right-[14px] translate-x-7'></span>
                             <div>Build and Create the Collection with your team using our intuitive tools designed for co-working. Easily share your workspace - together, or work solo. You decide with your personalized No Code Tools available with the App.</div>
-                        </div>
+                        </li>
 
-                        <div className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
+                        <li className='w-1/2 h-auto px-4 py-8 border border-white rounded-lg relative z-[99] mb-8 float-right clear-left translate-x-7'>
                             <h2 className='font-bold text-2xl'>Generate</h2>
+                            <span className='border border-black w-6 h-6 bg-white rounded-full absolute top-1/2 -translate-y-1/2 -left-[14px] -translate-x-7'></span>
                             <div>Upload, Arrange and Click Generate. Using the Tools you can seamlessly work and focus on other areas of building your NFT Project. Easily move across generating and building out the Contracts for your proposed NFT Collection. Generating has never been more efficient.</div>
-                        </div>
-                    </div>
-                    
-                    <div className='px-2 bg-red-300 relative'>
-                    </div>
-                    
-                    <div className='flex-1 space-y-4'>
-                        {/* card */}
-                        <div className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5 relative'>
-                            <h2 className='font-bold text-2xl'>The Software</h2>
-                            <div>A No Code Tool for Creators to be able to build , create and generate NFTs. Experience a seamless transition into NFTs and Web3 using the Generate Labs Software Application.</div>
-                            <div className='w-4 h-4 rounded-full bg-blue-300 absolute left-0 top-full '></div>  
-                        </div>
-
-                        <div className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
-                            <h2 className='font-bold text-2xl'>Build</h2>
-                            <div>Envision your ideas and seamlessly build the NFT Collection and Creation that you have in mind. The possibilities are endless using the Generate Labs No Code Tool and Software.</div>
-                        </div>
-
-                        <div className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
-                            <h2 className='font-bold text-2xl'>Create</h2>
-                            <div>Build and Create the Collection with your team using our intuitive tools designed for co-working. Easily share your workspace - together, or work solo. You decide with your personalized No Code Tools available with the App.</div>
-                        </div>
-
-                        <div className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
-                            <h2 className='font-bold text-2xl'>Generate</h2>
-                            <div>Upload, Arrange and Click Generate. Using the Tools you can seamlessly work and focus on other areas of building your NFT Project. Easily move across generating and building out the Contracts for your proposed NFT Collection. Generating has never been more efficient.</div>
-                        </div>
-                    </div>
+                        </li>
+                    </ul>
                 </div>
 
             </Padding>
@@ -65,3 +46,18 @@ const Development: FunctionComponent = () => {
 }
 
 export default Development
+
+// <li className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
+// <h2 className='font-bold text-2xl'>Build</h2>
+// <div>Envision your ideas and seamlessly build the NFT Collection and Creation that you have in mind. The possibilities are endless using the Generate Labs No Code Tool and Software.</div>
+// </li>
+
+// <li className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
+// <h2 className='font-bold text-2xl'>Create</h2>
+// <div>Build and Create the Collection with your team using our intuitive tools designed for co-working. Easily share your workspace - together, or work solo. You decide with your personalized No Code Tools available with the App.</div>
+// </li>
+
+// <li className='border border-white px-4 py-8 text-white rounded-lg font-montserrat space-y-5'>
+// <h2 className='font-bold text-2xl'>Generate</h2>
+// <div>Upload, Arrange and Click Generate. Using the Tools you can seamlessly work and focus on other areas of building your NFT Project. Easily move across generating and building out the Contracts for your proposed NFT Collection. Generating has never been more efficient.</div>
+// </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,8 +21,8 @@ module.exports = withMT({
             blackText: '#28292B',
             dirtyWhite: '#F5F5F5'
         },
-        spacing: {
-            
+        backgroundImage: {
+            'line-gradient': 'linear-gradient(180deg, #2E2929 5.43%, #B7886A 12.69%, #D2886E 19.18%, #CE503E 26%, #A25256 32.93%, #746681 40.09%, rgba(64, 109, 158, 0.19) 51.57%, rgba(255, 255, 255, 1) 60.03%)'
         }
     },
   },
