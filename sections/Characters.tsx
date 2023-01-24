9290888c07a848bd415e6ca314185c619fa3037a
Lyzer <mercklyzer@gmail.com>
2023-01-24 14:48:21 +0800

    Attempt Optimization 1

--- sections/Characters.tsx
+++ sections/Characters.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React, { FunctionComponent } from 'react'
 import Padding from '../components/Padding'
 
@@ -15,22 +16,22 @@ const Characters: FunctionComponent = () => {
             >
                 <div className='grid grid-rows-2 grid-cols-3 gap-2 gap-y-6
                     md:grid-cols-4'>
-                    <img src="/images/nft_card_1.svg" alt="NFT Card"/>
-                    <img src="/images/nft_card_2.svg" alt="NFT Card"/>
-                    <img src="/images/nft_card_3.svg" alt="NFT Card"/>
+                    <Image width={203} height={311} src="https://generate-labs-nextjs.vercel.app/images/nft_card_1.svg" priority alt="NFT Card"/>
+                    <Image width={203} height={311} src="https://generate-labs-nextjs.vercel.app/images/nft_card_2.svg" priority alt="NFT Card"/>
+                    <Image width={203} height={311} src="https://generate-labs-nextjs.vercel.app/images/nft_card_3.svg" priority alt="NFT Card"/>
                     
-                    <img src="/images/nft_card_4.svg" alt="NFT Card"/>
-                    <img src="/images/nft_card_5.svg" alt="NFT Card"/>
-                    <img src="/images/nft_card_6.svg" alt="NFT Card"/>
+                    <Image width={203} height={311} src="https://generate-labs-nextjs.vercel.app/images/nft_card_4.svg" priority alt="NFT Card"/>
+                    <Image width={203} height={311} src="https://generate-labs-nextjs.vercel.app/images/nft_card_5.svg" priority alt="NFT Card"/>
+                    <Image width={203} height={311} src="https://generate-labs-nextjs.vercel.app/images/nft_card_6.svg" priority alt="NFT Card"/>
 
-                    <img className="hidden md:block" src="/images/nft_card_7.svg" alt="NFT Card"/>
-                    <img className="hidden md:block" src="/images/nft_card_8.svg" alt="NFT Card"/>
+                    <Image width={203} height={311} className="hidden md:block" src="https://generate-labs-nextjs.vercel.app/images/nft_card_7.svg" priority alt="NFT Card"/>
+                    <Image width={203} height={311} className="hidden md:block" src="https://generate-labs-nextjs.vercel.app/images/nft_card_8.svg" priority alt="NFT Card"/>
                 </div>
 
                 <div className='flex items-center justify-center w-full mt-6 space-x-2 
                     md:hidden'>
-                    <img className="w-1/3" src="/images/nft_card_7.svg" alt="NFT Card"/>
-                    <img className="w-1/3" src="/images/nft_card_8.svg" alt="NFT Card"/>
+                    <Image width={203} height={311} className="w-1/3" src="https://generate-labs-nextjs.vercel.app/images/nft_card_7.svg" priority alt="NFT Card"/>
+                    <Image width={203} height={311} className="w-1/3" src="https://generate-labs-nextjs.vercel.app/images/nft_card_8.svg" priority alt="NFT Card"/>
                 </div>
 
             </Padding>
