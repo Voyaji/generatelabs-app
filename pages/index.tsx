3ebb58b7de190038645718d82be20806c675ed73
Lyzer <mercklyzer@gmail.com>
2023-01-24 12:47:22 +0800

    Responsive Newsletter

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
             <Development />
             <Faq />
             <Brands />
-            {/* <Newsletter /> */}
+            <Newsletter />
 
         </main>
     )
--- sections/Newsletter.tsx
+++ sections/Newsletter.tsx
@@ -7,25 +7,34 @@ const Newsletter: FunctionComponent = () => {
             style={{
                 backgroundImage: `url('/images/gradient_section_bg.svg')`,
                 backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover'
+                backgroundSize: 'cover',
+                backgroundPosition: '75%'
             }}>
 
-            <Padding className='pt-16 pb-7'>
-                <div className='flex'>
-                    <div className='flex flex-col space-y-4'>
-                        <span className='font-montserrat text-xl text-blackText'>Sign up for</span>
-                        <h1 className='text-white font-spaceGrotesk font-bold text-7xl'>NEWSLETTER</h1>
-                        <span className='font-montserrat text-xl text-blackText'>Are you a Creator Interested to build, create and generate your own NFTs? Sign up for Beta by following the  link provided. </span>
+            <Padding className='!pt-16 !pb-7'>
+                <div className='flex flex-col
+                    lg:flex-row lg:space-x-8'>
+                    <div className='flex flex-col space-y-4 items-center
+                        lg:items-start'>
+                        <span className='font-montserrat text-xl text-blackText font-bold 
+                            lg:font-normal'>Sign up for</span>
+                        <h1 className='text-white font-spaceGrotesk font-bold text-4xl
+                            md:text-5xl
+                            xl:text-7xl'>NEWSLETTER</h1>
+                        <span className='font-montserrat text-lg  text-blackText
+                            lg:text-xl'>Are you a Creator Interested to build, create and generate your own NFTs? Sign up for Beta by following the  link provided. </span>
                     </div>
 
-                    <div className='flex flex-col justify-center items-end w-full'>
+                    <div className='flex flex-col justify-center items-center w-full mt-12
+                        lg:items-end lg:mt-0'>
                         <form className='space-y-11'>
                             <div className='flex flex-col items-end space-y-5 w-full'>
-                                <input className='font-montserrat text-black text-base px-5 py-3 rounded-lg w-96' type="text" placeholder='Your name' />
-                                <input className='font-montserrat text-black text-base px-5 py-3 rounded-lg w-96' type="email" placeholder='E-mail Address' />
+                                <input className='font-montserrat text-black placeholder-black text-base px-5 py-3 rounded-lg w-60 sm:w-96' type="text" placeholder='Your name' />
+                                <input className='font-montserrat text-black placeholder-black text-base px-5 py-3 rounded-lg w-60 sm:w-96' type="email" placeholder='E-mail Address' />
                             </div>
 
-                            <div className='flex justify-start w-full'>
+                            <div className='flex justify-center w-full
+                                lg:justify-start'>
                                 <button className='bg-black text-white px-11 py-5 rounded-lg text-xl font-inter'>
                                     Sign Up
                                 </button>
@@ -43,4 +52,4 @@ const Newsletter: FunctionComponent = () => {
     )
 }
 
-export default Newsletter
+export default Newsletter;
